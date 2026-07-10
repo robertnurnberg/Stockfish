ef30277813f358a607ac0d3c2af7d5526f77c056
Robert Nurnberg @ elitebook <28635489+robertnurnberg@users.noreply.github.com>
2026-07-10 11:06:24 +0200

    take 2: d offset 4

--- src/thread.cpp
+++ src/thread.cpp
@@ -365,7 +365,7 @@ Thread* ThreadPool::get_best_thread() const {
     // Vote according to score and depth, and select the best thread
     for (auto&& th : threads)
         votes[th->worker->rootMoves[0].pv[0]] += (th->worker->rootMoves[0].score - minScore + 14)
-                                               * int(th->worker->rootDepth - minDepth + 8);
+                                               * int(th->worker->rootDepth - minDepth + 4);
     ;
 
     for (auto&& th : threads)
