cf03d42c9362d649ff5e02bf39a230adf560b88d
Robert Nurnberg @ elitebook <28635489+robertnurnberg@users.noreply.github.com>
2026-09-22 09:31:43 +0200

    Bench: 1796265

--- src/search.cpp
+++ src/search.cpp
@@ -736,7 +736,10 @@ Value Search::Worker::search(
     constexpr bool PvNode   = nodeType != NonPV;
     constexpr bool rootNode = nodeType == Root;
     const bool     allNode  = !(PvNode || cutNode);
-    const bool     seekMate = rootDepth >= 16 && std::abs(rootMoves[pvIdx].score) >= 2000;
+
+    // Do not tune these values. They are not intended for playing strength.
+    const bool seekMate =
+      std::abs(rootMoves[pvIdx].score) >= 750 + 220000 / (rootDepth * rootDepth);
 
     // Dive into quiescence search when the depth reaches zero
     if (depth <= 0)
